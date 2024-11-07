cda13004b8bccb70ed6a5dd546b14797730ce2c0
PowerOleg <geraskin.oleg@mail.ru>
2024-11-07 12:35:57 +0300

    need to define constructor

--- Builder/main.cpp
+++ Builder/main.cpp
@@ -6,13 +6,14 @@
 int main(int argc, char** argv) {
     SqlSelectQueryBuilder query_builder;
     query_builder.AddColumn("name").AddColumn("phone");
-    //query_builder.AddWhere("id", "42");
+    query_builder.AddWhere("id", "42");
+    query_builder.AddWhere("name", "John");
+    /*constexpr */char* result = query_builder.BuildQuery();
+
+
 
     //query_builder.AddFrom("students");
-    //query_builder.AddWhere("id", "42").AddWhere("name", "John");
-    
-    //constexpr bool result = query_builder.BuildQuery();
-
+//query_builder.AddWhere("id", "42").AddWhere("name", "John");
     //static_assert(result,
     //                "SELECT name, phone FROM students WHERE id=42 AND name=John;");
 
--- Builder/sql_query_builder.h
+++ Builder/sql_query_builder.h
@@ -2,45 +2,87 @@
 #include <string>
 #include <vector>
 #include <deque>
-
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
 
 class SqlSelectQueryBuilder
 {
 
 public:
-	//constexpr bool BuildQuery()
-	constexpr char* BuildQuery()
+
+	SqlSelectQueryBuilder()//нужно сделать поле через которое будет передаваться AddColumn значение из одного объекта в другой
+
+
+
+	/*constexpr*/ char* BuildQuery()
 	{
 		char* result = new char[256] {};
 		//id = 42 AND name = John; "
-		strcat(result, select);
 
-		const char* select1 = columns.back();
-
-		/*std::to_string*/
-		//strcat(select, );
+		strcpy(result, select);
 		
+		if (columns.size() > 0)
+		{
+			for (size_t i = 0; i < columns.size(); i++)
+			{
+				const char* column_var = columns.front().c_str();
+				strcat(result, column_var);
+				strcat(result, ",");
+			}
+			char* ptr = strrchr(result, ',');
+			*ptr = 0;
+			//проверка на длинну
+		}
+		else
+		{
+			const char* column_var = "* ";
+			strcat(result, column_var);
+		}
+
+		strcat(result, from);
+
+
+		for (size_t i = 0; i < where_names.size(); i++)
+		{
+			const char* where_name1 = where_names.front().c_str();
+			const char* where_value1 = where_values.front().c_str();
+			strcat(result, where_name1);
+			strcat(result, " = ");
+			strcat(result, where_value1);
+
+			if (i + 1 < columns.size())
+			{
+				strcat(result, and_);
+			}
+			
+			//проверка на длинну
+		}
+		
+		strcat(result, ";");
+
 		return result;
 	}
 
-	SqlSelectQueryBuilder AddColumn(const char* column)
+	SqlSelectQueryBuilder AddColumn(const std::string column)
 	{
 		columns.push_back(column);
-		return SqlSelectQueryBuilder();
+		return SqlSelectQueryBuilder(columns);
 	}
 
-	//SqlSelectQueryBuilder AddWhere(std::string name, std::string value)
-	//{
-	//	where_names.push_back(name);
-	//	where_values.push_back(value);
-	//}
+	SqlSelectQueryBuilder AddWhere(const std::string name, const std::string value)
+	{
+		where_names.push_back(name);
+		where_values.push_back(value);
+		return SqlSelectQueryBuilder();
+	}
 	
 
 private:
-	std::vector<const char*> columns;
+	std::vector<std::string> columns;
 	std::vector<std::string> where_names;
 	std::vector<std::string> where_values;
 	const char* select  = "SELECT ";
-	const char* from = "FROM students WHERE ";
+	const char* from = " FROM students WHERE ";
+	const char* and_ = " AND ";
 };
 
